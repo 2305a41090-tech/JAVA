86c2d18cbf736fa51617d0275b81776e57758a87
2305a41090-tech <2305a41090@sru.edu.in>
2025-12-10 16:11:07 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,41 @@
+/* 
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+        System.out.print("enter the name");
+        String str=sc.nextLine();
+        System.out.println(str);
+    }
+    
+}
+    */
+   import java.util.Scanner;
+   public class userinput {
+    public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       System.out.print("enter the a");
+       int a=sc.nextInt();
+       System.out.print("enter the b");
+       int b=sc.nextInt();
+       System.out.println("assign operation");
+       char op =sc.next().charAt(0);
+       if(op=='+'){
+        int sum=a+b;
+        System.out.println("addition is +add");
+       }
+       else if(op=='-'){
+        System.out.println("substration");
+       }
+        else if(op=='*'){
+            System.out.println("mutlpication");
+        }
+        else if(op=='/'){
+                System.out.println("divison");
+            }
+        }
+       }
+    
+
+
+
