0d40d9ae454afe7ccea3573ea43a69bed85309ea
2305a41090-tech <2305a41090@sru.edu.in>
2025-12-11 11:49:29 +0530

    Add files via upload

---
+++ fuction.java
@@ -0,0 +1,40 @@
+/* 
+public class fuction {
+    public static void main(String[]args){
+        System.out.println(add(2,3));
+    }
+
+        public static int add(int num1,int num2){
+           
+ return num1+num2   ;    
+         }
+    }
+         */
+        public class fuction {
+    public static void main(String[]args){
+        System.out.println(add(2,3));
+        System.out.println(sub(2,3));
+        System.out.println(mult(2,3));
+        System.out.println(divi(2,3));
+
+
+
+    }
+
+        public static int add(int num1,int num2){
+           
+ return num1+num2;   
+         }
+         public static int sub(int num1,int num2){
+            return num1-num2;
+
+         }
+         public static int mult(int num1,int num2){
+            return num1*num2;
+
+         }
+         public static int divi(int num1,int num2){
+            return num1/num2;
+    }
+}
+
