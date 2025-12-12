83be5476f93b27035783d54e7a8a7f37f377e51b
2305a41090-tech <2305a41090@sru.edu.in>
2025-12-12 09:27:18 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,16 @@
+public class exception {
+    public static void main(String[]args){
+       try {
+        int a=20;
+        int b=0;
+        int c=a/b;
+        System.out.println("Result:"+c);
+       }
+       catch(Exception Akshitha){
+        System.out.println("caught exception:"+Akshitha.getMessage());
+       }
+    }
+}
+
+    
+
